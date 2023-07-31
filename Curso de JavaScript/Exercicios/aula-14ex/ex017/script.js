6a7598fd02853f3438339f2895fd563d8ff26a51
Rafael Gregório de Oliveira <rafagre@live.com>
2023-07-31 18:06:20 -0300

    Adição de novas aulas e exercicios

---
+++ Curso de JavaScript/Exercicios/aula-14ex/ex017/script.js
@@ -0,0 +1,11 @@
+function tabuada() {
+    let n = document.getElementById('txtn')
+    let t = document.getElementById('seltab')
+
+    if (n.value.length == '') {
+        window.alert('Incira um numero.')
+        //tratamento de erro
+    } else {
+
+    }
+}
