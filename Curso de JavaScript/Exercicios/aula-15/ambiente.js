8c02fbf34618a855e0c3acfc4926011516fb9e2f
Rafael Gregório de Oliveira <rafagre@live.com>
2023-08-22 19:04:03 -0300

    adição de novos exercicios

---
+++ Curso de JavaScript/Exercicios/aula-15/ambiente.js
@@ -0,0 +1,14 @@
+/*let num = [5, 8, 4]
+num[3] = 6 // acrecenta falor no local certo.
+num.push(7) //acrecenta valor em local aleatório
+num.length // descreve qual o comprimento do vetor
+num.sort() //coloca os elementos de forma organizada*/
+
+let num = [5, 8, 2, 9, 3]
+num.sort
+num.push(1)
+console.log(num)
+console.log(`O valor tem ${num.length} posições`)
+console.log(`O primeiro valor do vetor é ${num[0]}`)
+let pos = num.indexOf(8)
+console.log(`O valor 8 está na posição ${pos}`)
